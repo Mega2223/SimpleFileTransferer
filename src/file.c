85e623ba955c87a5a449ae6ea4d4eb3d14b8565c
mega2223 <juliocesarborgesdeoliveir@gmail.com>
2026-05-03 02:40:38 -0300

    ta temos o flatten

--- src/file.c
+++ src/file.c
@@ -1,6 +1,7 @@
 #ifndef FILE_C
 #define FILE_C
 
+#include "file.h"
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,8 +14,17 @@
 
 #define SEND_BUFFER_SIZE 64
 
-#define FILE_SEND_BEGIN '\7'
-#define FILE_SEND_ESCAPE '\10'
+typedef struct known_file {
+    size_t fname_len;
+    long file_size;
+    char* fname;
+    struct known_file* next;
+} known_file;
+
+typedef struct trans_header {
+    int n_files;
+    known_file* files;
+} trans_header;
 
 void sendHeader(int stream_fileno, const char* dir_path);
 
@@ -30,7 +40,7 @@ long fileSize(const char* file_name) {
 
 void concatenate_path(char* dest, const char* dir, const char* filename)
 {
-    int p_len = strlen(dir), f_len = strlen(filename); 
+    size_t p_len = strlen(dir), f_len = strlen(filename); 
     dest[p_len + f_len + 1] = '\0';
     strcpy(dest, dir);
     dest[p_len] = '/';
@@ -51,11 +61,11 @@ void sendFile(int stream_fileno, const char* file_path)
     char send_buffer[SEND_BUFFER_SIZE];
     bzero(send_buffer, SEND_BUFFER_SIZE);
 
-    int n;
+    size_t n;
     do {
         bzero(send_buffer, SEND_BUFFER_SIZE);
-        n = read(fno, send_buffer, SEND_BUFFER_SIZE);
-		printf("Read %d bytes\n", n);
+        n = (size_t) read(fno, send_buffer, SEND_BUFFER_SIZE);
+		printf("Read %ld bytes\n", n);
     	write(stream_fileno, send_buffer, n);
     } while (n > 0);
 	printf("reddit\n");
@@ -95,42 +105,72 @@ void sendDir(int stream_fileno, const char* dir_path)
 
 const char format[] = "DIR:\"%s\"\n";
 
+int getAllFiles(const char* f_path, known_file* next_file)
+{
+    printf("at call %s\n", f_path);
+    DIR* dir = opendir(f_path);
+    if (dir != NULL) {
+        // Is dir
+        int file_count = 0;
+        struct dirent* de = NULL;
+        do {
+            de = readdir(dir);
+            if (de == NULL || de->d_name[0] == '.')
+                continue;
+            char nf_path[strlen(f_path) + strlen(de->d_name) + 1];
+            concatenate_path(nf_path, f_path, de->d_name);
+            printf("looking file \"%s\" (\"%s\",\"%s\")\n", nf_path, f_path, de->d_name);
+            int f_files = getAllFiles(nf_path, next_file);
+            if (f_files == -1){
+                file_count++;
+                next_file = next_file->next;
+            } else {
+                for (int i = 0; i < f_files; i++) { // This is so dumb i love it
+                    printf("Exchanging %p to %p\n", next_file, next_file->next);
+                    next_file = next_file->next;
+                }
+            }
+        } while (de != NULL);
+        printf("call (%s) returning %d\n",f_path,file_count);
+        return file_count;
+    } else if (errno == ENOTDIR) {
+        // Is file
+        char* n_name = malloc(strlen(f_path) + 1);
+        bzero(n_name, strlen(f_path) + 1);
+        strcpy(n_name, f_path);
+        next_file->fname = n_name;
+        next_file->file_size = fileSize(n_name);
+        next_file->next = malloc(sizeof(known_file));
+        next_file->next->file_size = 0;
+        printf("setting %p to file %s, next %p\n",next_file,n_name,next_file->next);
+        return -1;
+    } else {
+        printf("Error for file %s\n", f_path);
+        return -2;
+	}
+}
+
+void printFileChain(known_file* first)
+{
+    known_file* cur = first;
+    printf("\n");
+    while (cur != NULL) {
+        // printf("\"%s\" <%p> [%ld] -> ",cur->fname, cur, cur->file_size);
+        printf("%s>",cur->fname);
+        cur = cur->next;
+    }
+    printf("\n");
+}
+
+
 void sendHeader(int stream_fileno, const char* dir_path)
 {
-    DIR* dir = opendir(dir_path);
-    int name_len = strlen(dir_path);
-    if (dir != NULL) {
-        int bsize = name_len + strlen(format) + 1;
-        char buffer[bsize];
-        bzero(buffer, bsize);
-        sprintf(buffer, format, dir_path);
-        write(stream_fileno, buffer, strlen(buffer));
-        
-        struct dirent* f = readdir(dir);
-        while (f != NULL) {
-            if (f->d_name[0] == '.') {
-                f = readdir(dir);
-                continue;
-            }
-            char ndir[name_len + strlen(f->d_name) + 1];
-            concatenate_path(ndir, dir_path, f->d_name);
-            sendHeader(stream_fileno, ndir);
-            f = readdir(dir);
-        }
-
-    } else if (errno == ENOTDIR) {
-        write(stream_fileno, "FSIZE=", 5);
-        long fsize = fileSize(dir_path);
-        char fsize_s[128];
-        bzero(fsize_s, 128);
-        sprintf(fsize_s, "%ld", fsize);
-        write(stream_fileno, fsize_s, strlen(fsize_s));
-        write(stream_fileno, "\n", 1);
-
-    } else {
-		printf("Error when reading file \"%s\" for header: %d\n", dir_path, errno);
-		exit(-1);
-	}
+    trans_header header;
+    known_file* first = malloc(sizeof(known_file));
+    printf("\n POINTER %p \n", first);
+    getAllFiles(dir_path, first);
+    printf("\n POINTER %p \n", first);
+    printFileChain(first);    
 }
 
 #endif
--- src/file.h
+++ src/file.h
@@ -1,7 +1,11 @@
 #ifndef FILE_H
 #define FILE_H
 
+#include <stdlib.h>
+
 void sendFile(int stream_fileno, const char* file_path);
 void sendDir(int stream_fileno, const char* dir_path);
+
+// void printFileChain(known_file* first);
 
 #endif
